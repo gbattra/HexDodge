ec5199c44c04558122eaef7384cc44b3da2ab69f
Gregory Broderick Attra <greg.attra@gmail.com>
2020-01-25 14:00:16 -0500

    added high score to menu

--- Assets/Scripts/GameModeMenu.cs
+++ Assets/Scripts/GameModeMenu.cs
@@ -1,11 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameModeMenu : MonoBehaviour
 {
     public GameObject MainMenu;
+
+    public TextMeshProUGUI BurstHighScore;
+    public TextMeshProUGUI SprintHighScore;
+    public TextMeshProUGUI LongDistanceHighScore;
+    public TextMeshProUGUI MarathonHighScore;
+    public TextMeshProUGUI EndlessHighScore;
+
+    public void Awake()
+    {
+        BurstHighScore.text = PlayerPrefs.GetInt("Burst_highScore", 0).ToString();
+        SprintHighScore.text = PlayerPrefs.GetInt("Sprint_highScore", 0).ToString();
+        LongDistanceHighScore.text = PlayerPrefs.GetInt("LongDistance_highScore", 0).ToString();
+        MarathonHighScore.text = PlayerPrefs.GetInt("Marathon_highScore", 0).ToString();
+        EndlessHighScore.text = PlayerPrefs.GetInt("Endless_highScore", 0).ToString();
+    }
 
     public void GoBack()
     {
