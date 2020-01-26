b122f5b41aa30ddb2981ca82fbf3db4cfd003922
Gregory Broderick Attra <greg.attra@gmail.com>
2020-01-26 15:22:36 -0500

    updated canvas ui

--- Assets/Scripts/LevelCanvas.cs
+++ Assets/Scripts/LevelCanvas.cs
@@ -21,7 +21,7 @@ public class LevelCanvas : MonoBehaviour
             {
                 StartCoroutine(AnnounceHighScore());
                 FindObjectOfType<AudioManager>().Play("AnnounceHighScore");
-                _counter.GetComponent<Tracker>().Value.GetComponent<Text>().color = Color.cyan;
+                TileTracker.GetComponent<Tracker>().Value.GetComponent<Text>().color = Color.cyan;
                 _hasNewHighScore = value;
             }
         }
@@ -31,53 +31,27 @@ public class LevelCanvas : MonoBehaviour
     public GameObject BoostBar;
     public GameObject RocketBar;
     public GameObject HealthBar;
-    public GameObject TrackerPrefab;
-    public GameObject RestartButtonPrefab;
+    public GameObject TimeTracker;
+    public GameObject TileTracker;
+    public GameObject RestartButton;
     
     public GameObject NewHighScoreText;
     public GameObject MuteButton;
     public GameObject UnmuteButton;
 
     public TextMeshProUGUI TimeRemainingText;
-    
-    public Vector3 TimerPosition;
-    public Vector3 CounterPosition;
-    public Vector3 RestartButtonPosition;
 
-    public GameObject Timer => _timer;
-    private GameObject _timer;
-
-    public GameObject Counter => _counter;
-    private GameObject _counter;
-
-    public GameObject RestartButton => _restartButton;
-    private GameObject _restartButton;
-
-    public GameObject GameOverPrefab;
-    public GameObject GameOverSprite => _gameOverSprite;
+    public GameObject GameOverSprite;
 
     private bool IsYellow;
     private bool IsRed;
 
-    private GameObject _gameOverSprite;
 
     public void Awake()
     {
-        _gameOverSprite = Instantiate(GameOverPrefab, transform, false);
-        _gameOverSprite.SetActive(false);
-
-        _timer = Instantiate(TrackerPrefab, transform, false);
-        _timer.GetComponent<Tracker>().SetLabelAndValue("TIMER", "0:00");
-        _timer.transform.position += TimerPosition;
-
-        _counter = Instantiate(TrackerPrefab, transform, false);
-        _counter.GetComponent<Tracker>().SetLabelAndValue("TILES", "0");
-        _counter.transform.position += CounterPosition;
-
-        _restartButton = Instantiate(RestartButtonPrefab, transform, false);
-        _restartButton.transform.position += RestartButtonPosition;
-        _restartButton.GetComponent<Button>().onClick.AddListener(Restart);
-        _restartButton.SetActive(false);
+        TimeTracker.GetComponent<Tracker>().SetLabelAndValue("TIMER", "0:00");
+        TileTracker.GetComponent<Tracker>().SetLabelAndValue("TILES", "0");
+        RestartButton.GetComponent<Button>().onClick.AddListener(Restart);
     }
 
     public void SetTimerColor(Color color)
@@ -85,7 +59,7 @@ public class LevelCanvas : MonoBehaviour
         if (!IsYellow && color == Color.yellow)
         {
             StartCoroutine(AnnounceTimeRemaining(20));
-            _timer.GetComponent<Tracker>().SetColor(color);
+            TimeTracker.GetComponent<Tracker>().SetColor(color);
             IsYellow = true;
         }
 
@@ -93,7 +67,7 @@ public class LevelCanvas : MonoBehaviour
         {
             StartCoroutine(AnnounceTimeRemaining(10));
             StartCoroutine("FinalCountdown");
-            _timer.GetComponent<Tracker>().SetColor(color);
+            TimeTracker.GetComponent<Tracker>().SetColor(color);
             IsRed = true;
         }
     }
@@ -159,12 +133,12 @@ public class LevelCanvas : MonoBehaviour
 
     public void SetTileCount(int count)
     {
-        _counter.GetComponent<Tracker>().SetLabelAndValue("TILES", count.ToString());
+        TileTracker.GetComponent<Tracker>().SetLabelAndValue("TILES", count.ToString());
     }
 
     public void SetTime(string value)
     {
-        _timer.GetComponent<Tracker>().SetLabelAndValue(
+        TileTracker.GetComponent<Tracker>().SetLabelAndValue(
             "TIMER",
             value);
     }
--- Assets/Scripts/Tracker.cs
+++ Assets/Scripts/Tracker.cs
@@ -1,45 +1,23 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Tracker : MonoBehaviour
 {
-    public string LabelText;
-    public string ValueText;
-    
-    public GameObject BackgroundPrefab;
-    public GameObject LabelPrefab;
-    public GameObject ValuePrefab;
-
-    public GameObject Background => _background;
-    private GameObject _background;
-
-    public GameObject Label => _label;
-    private GameObject _label;
-
-    public GameObject Value => _value;
-    private GameObject _value;
-
-    public void Awake()
-    {
-        _background = Instantiate(BackgroundPrefab, transform, false);
-        _label = Instantiate(LabelPrefab, transform, false);
-        _value = Instantiate(ValuePrefab, transform, false);
-        
-        _label.GetComponent<Text>().text = LabelText;
-        _value.GetComponent<Text>().text = ValueText;
-    }
+    public GameObject Label;
+    public GameObject Value;
 
     public void SetLabelAndValue(string label, string value)
     {
-        _label.GetComponent<Text>().text = label;
-        _value.GetComponent<Text>().text = value;
+        Label.GetComponent<TextMeshProUGUI>().text = label;
+        Value.GetComponent<TextMeshProUGUI>().text = value;
     }
 
     public void SetColor(Color color)
     {
-        _value.GetComponent<Text>().color = color;
+        Value.GetComponent<TextMeshProUGUI>().color = color;
     }
 }
